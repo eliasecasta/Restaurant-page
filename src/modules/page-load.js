5f4c77159c3e375e124d67e90c2d21759c1ae697
Elias Castañeda <e71az@icloud.com>
2021-01-20 06:18:53 -0700

    Nav creation functions

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -1,11 +1,34 @@
-function navElements(className) {
-  
+function navElements(id, content) {
+
+  const navElement = document.createElement('a')
+  navElement.setAttribute('id', id)
+  navElement.setAttribute('href', '#')
+  navElement.classList.add('navElement')
+  navElement.textContent = content
+  return navElement
+}
+
+function createNav(id) {
+  const navbar = document.createElement('nav')
+  navbar.setAttribute('id', id)
+
+  const pageName = navElements('home', 'Le Buffet')
+  const menu = navElements('menu', 'Menu')
+  const contact = navElements('contact', 'Contact')
+
+  navbar.appendChild(pageName)
+  navbar.appendChild(menu)
+  navbar.appendChild(contact)
+
+  return navbar
 }
 
 function pageLoad() {
   const content = document.getElementById('content')
-  const header = newHeader('header', 'Paradise food')
-  content.appendChild(header)
+
+  const navbar = createNav('nav')
+  content.appendChild(navbar)
+
 }
 
 export default pageLoad
